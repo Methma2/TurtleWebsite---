bad9c5e9d1f51e8a01323124043a2981c0a152cd
Methma2 <135090879+Methma2@users.noreply.github.com>
2023-08-14 12:06:56 +0530

    Add files via upload

---
+++ summary.js
@@ -0,0 +1,6 @@
+document.getElementById('summaryDate').innerHTML =  localStorage.getItem('summaryDate');
+    document.getElementById('summaryTime').innerHTML = localStorage.getItem('summaryTime');
+    document.getElementById('summaryDuration').innerHTML = localStorage.getItem('summaryDuration');
+    document.getElementById('summaryTickets').innerHTML = localStorage.getItem('summaryTickets');
+    document.getElementById('summaryTotal').innerHTML = localStorage.getItem('summaryTotal');
+
